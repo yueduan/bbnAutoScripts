5a50c7116efba807eb981183ad36e63b65f4ad0d
yueduan <bestdyxjtu@gmail.com>
2017-05-15 22:01:54 -0700

    Update launch_KillApp.sh

--- launch_KillApp.sh
+++ launch_KillApp.sh
@@ -5,6 +5,7 @@ launch=$2
 
 packageName=$(/home/yduan/yueduan/android-5.0.0_r3/out/host/linux-x86/bin/aapt dump badging $apk | awk -v FS="'" '/package: name=/{print $2}')
 
+echo "Package Name: $packageName"
 if [ "$launch" = "1" ]
 then
 	adb shell monkey -p $packageName -c android.intent.category.LAUNCHER 1
